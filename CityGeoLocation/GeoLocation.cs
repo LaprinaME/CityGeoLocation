f8d14229152db12aa0bb5294297033c368843a2e
LaprinaME <LaprinaME@206D-2>
2023-03-16 15:17:15 +0700

    Добавьте файлы проекта.

---
+++ CityGeoLocation/GeoLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CityGeoLocation
+{
+    public class GeoLocation
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
